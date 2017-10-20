e8713664509b0234d3e1cd2863a83026a1c03ce5
v-vishal <reach2vishalv@gmail.com>
2017-10-20 22:31:50 +0530

    Project Setup

---
+++ MobiKart-BackEnd/src/main/java/com/NIIT/MobiKart_BackEnd/App.java
@@ -0,0 +1,13 @@
+package com.NIIT.MobiKart_BackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
